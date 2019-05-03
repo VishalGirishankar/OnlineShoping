569d7c0de39564a6721414f97db373cb69a234a3
vishal.girishankar <vishal.girishankar@Aspire1091.Aspiresys.com>
2019-05-03 16:34:39 +0530

    Second Commit

---
+++ onlineshoping/src/main/java/com/aspire/onlineshoping/models/dto/LoginReponse.java
@@ -0,0 +1,13 @@
+package com.aspire.onlineshoping.models.dto;
+
+public class LoginReponse {
+	private String loginStatus;
+
+	public String getLoginStatus() {
+		return loginStatus;
+	}
+
+	public void setLoginStatus(String loginStatus) {
+		this.loginStatus = loginStatus;
+	}
+}
